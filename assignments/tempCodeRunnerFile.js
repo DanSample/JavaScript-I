eeb0a04e1dae84d7c045769f46eaff628fcd2667
Dan Sample <danssample@gmail.com>
2019-03-27 18:36:48 -0700

    Finshed objects.js, Project complete

--- assignments/tempCodeRunnerFile.js
+++ assignments/tempCodeRunnerFile.js
@@ -1,5 +1,4 @@
-for (let i = 0; i < inventory.length; i++) {
-//     if (inventory[i][car_make] === 'BMW' || inventory[i][car_make] === 'Audi') {
-//         BMWAndAudi.push(inventory[i]['car_make'])
-//     }
-// }
+grandChild = {
+    //   'name': 'Sam',
+    //   'age': 30
+    // }
