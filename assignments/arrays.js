75a9fd445fbfc6a2bed9a49d9292bcb5b0e7b96a
Dan Sample <danssample@gmail.com>
2019-03-27 17:13:16 -0700

    Finshed function-conversion.js conversions

--- assignments/arrays.js
+++ assignments/arrays.js
@@ -67,18 +67,18 @@ let inventory = [{"id":1,"car_make":"Lincoln","car_model":"Navigator","car_year"
 
 // ==== Challenge 2 ====
 // The dealer needs the information on the last car in their inventory.  What is the make and model of the last car in the inventory?  Log the make and model into the console.
-// let newArr = inventory.slice(-1);
-// let lastCar = newArr[0];
-// console.log(`The last car in the lot is ${lastCar.car_make} ${lastCar.car_model}`);
+let newArr = inventory.slice(-1);
+let lastCar = newArr[0];
+console.log(`The last car in the lot is ${lastCar.car_make} ${lastCar.car_model}`);
 
 // ==== Challenge 3 ====
 // The marketing team wants the car models listed alphabetically on the website. Sort all the car model names into alphabetical order and log the results in the console
-// let carModels = [];
+let carModels = [];
 
-// for (let i = 0; i < inventory.length; i++) {
-//     carModels.push(inventory[i]['car_model'])
-// };
-// console.log(carModels.sort());
+for (let i = 0; i < inventory.length; i++) {
+    carModels.push(inventory[i]['car_model'])
+};
+console.log(carModels.sort());
 
 // ==== Challenge 4 ====
 // The accounting team needs all the years from every car on the lot. Create a new array from the dealer data containing only the car years and log the result in the console.
